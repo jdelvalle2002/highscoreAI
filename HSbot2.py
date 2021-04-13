fea22f57330ff25c1b4db7fb829087d5c655fe96
jdelvalle2002 <63021540+jdelvalle2002@users.noreply.github.com>
2021-04-12 20:40:55 -0400

    new file with q-learn

--- HSbot2.py
+++ HSbot2.py
@@ -20,33 +20,41 @@ def tablero_listo(tablero):
 
 env = gym.make('high-v0')
 
-initial = 100
+initial = 200
 intentos = 150
-
+#### EMPECEMOS A IMPLEMENTAR EL Q-ALG
 ptjs = []
 max_p = []
-coordis = env.pos_lugares[:]
-rewards = []
+'''
 def jugada(env):
-    action = random.sample(coordis,1)
+    action = random.sample(env.coord,1)
     return action[0]
-
-
+'''
+rewards_all = []
 def jugar(initial):
     end = 0
+    exploration_rate = 0.5
     for episode in range(initial):   
         valor, puesto = env.reset()
-        reward = 0
+        reward_current = 0
         for j in range(intentos): # con 150 tuve 10/10
-            action = jugada(coordis)
-            valor, r, puesto, info = env.step(action)
+            # Exploration-exploitation trade-off
+            exploration_rate_threshold = random.uniform(0, 1)
+            if exploration_rate_threshold > exploration_rate:
+                #action = np.argmax(q_table[state,:])
+                action = "c3" 
+                pass
+            else:
+                action = random.choice(env.coordenadas)            
             
+            #action = jugada(coordis)
+            valor, reward, puesto, info = env.step(action)
             if not puesto:
-                r = -5
+                reward = -5
                 #print("###MALA###")
-            if r > 7:    
-                print("reward:",r)    
-            reward += r
+            if reward > 7:    
+                print("reward:",reward)    
+            reward_current += reward
             if tablero_listo(env.matriz):
                 end +=1
                 print("TERMINADO")
@@ -56,7 +64,7 @@ def jugar(initial):
         
         env.render()
         
-        rewards.append(reward)
+        rewards_all.append(reward)
         #if reward == max(ptjs):
         #    max_p.append([reward,env.msj])
     return end
--- HighAI/High_AI/envs/high_env.py
+++ HighAI/High_AI/envs/high_env.py
@@ -15,7 +15,7 @@ class HighEnv(gym.Env):
         self.p = 0 # puntaje
         self.matriz = []
         self.msj = []
-        self.pos_lugares = ["a1","a2","a3","a4","a5","b1","b2","b3","b4","b5","c1", "c2", "c3", "c4", "c5", "d1", "d2", "d3", "d4", "d5", "e1", "e2", "e3", "e4", "e5"]
+        self.coordenadas = ["a1","a2","a3","a4","a5","b1","b2","b3","b4","b5","c1", "c2", "c3", "c4", "c5", "d1", "d2", "d3", "d4", "d5", "e1", "e2", "e3", "e4", "e5"]
         self.pa = 0 #puntaje anterior
         self.d = 0
     def dado(self):
@@ -24,7 +24,7 @@ class HighEnv(gym.Env):
         valor = a+b
         return valor        
     def reset(self):
-        self.pos_lugares = ["a1","a2","a3","a4","a5","b1","b2","b3","b4","b5","c1", "c2", "c3", "c4", "c5", "d1", "d2", "d3", "d4", "d5", "e1", "e2", "e3", "e4", "e5"]
+        self.coordenadas = ["a1","a2","a3","a4","a5","b1","b2","b3","b4","b5","c1", "c2", "c3", "c4", "c5", "d1", "d2", "d3", "d4", "d5", "e1", "e2", "e3", "e4", "e5"]
         self.pa = 0
         self.p = 0
         self.puesto = True
